0f4bb37647191cf103df09de17b06402b4cbed81
jmyager <jmyager6@gmail.com>
2018-12-04 23:39:08 -0500

    database route 1st commit

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -6,6 +6,8 @@ function capitalizeFirstLetter(string) {
 // Clear the page of any historical data
 $("#allLakes").empty();
 
+let currentURL = window.location.origin;
+
 // // Capture the click on a lake
 // $('#allLakes').on('click', 'a', function() {
 //     // Pull data value
@@ -16,9 +18,31 @@ $("#allLakes").empty();
 
 
 // Capture click on state in dropdown and redirect user to the state URL
-$("button").click(function () {
+$("#allStates").on("click", "button", function () {
     //pull data value and current url
     var currentURL = window.location.origin;
     var state = $(this).attr("data-id");
     window.location.replace(currentURL + "/states/" + state);
+})
+
+$("#refresh").on("click", function() {
+    let apiURL = "https://waterservices.usgs.gov/nwis/iv/?format=json&sites=02079490&period=PT72H&parameterCd=62614&siteType=LK&siteStatus=all";
+    $.ajax({
+        url: apiURL,
+        method: "GET"
+    })
+        .then(function (data) {
+            console.log(data);
+
+            $.ajax({
+                url: currentURL + "/api/usgs",
+                method: "PUT",
+                data: ({data : data})
+            })
+                .then(function (data) {
+                    alert("Data refresh successful!");
+                    console.log(data)
+                })
+
+        });
 })
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,3 @@
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
-
-// var lakeData = require("../data/lakeData");
 
 // Require all models
 var db = require("../models")();
@@ -18,27 +11,18 @@ if (process.env.MONGODB_URI) {
   db.connect(databaseUri);
 }
 
-
-
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 
-module.exports = function(app) {
+module.exports = function (app) {
   // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
-
-  // app.get("/api/lakes", function(req, res) {
-  //   res.json(lakeData);
-  // });
 
   // Route to retrieve a single lake's data from db
   app.get("/api/lakes/:lakeName", function (req, res) {
     let lakeName = req.params.lakeName
-    db.model("Lake").find({name: lakeName})
+    db.model("Lake").find({ name: lakeName })
       .exec(function (err, data) {
         if (err) {
           res.send(lakeName + " lake data not found");
@@ -49,9 +33,9 @@ module.exports = function(app) {
   })
 
   //Route to retrieve lakes in a specific state
-  app.get("/api/states/:stateInitial", function(req, res) {
+  app.get("/api/states/:stateInitial", function (req, res) {
     let stateInitial = req.params.stateInitial;
-    db.model("Lake").find({state: stateInitial})
+    db.model("Lake").find({ state: stateInitial })
       .exec(function (err, data) {
         if (err) {
           res.send("No data found for " + state);
@@ -60,5 +44,23 @@ module.exports = function(app) {
         }
       })
   })
+
+  // API POST Requests
+  // ---------------------------------------------------------------------------
+
+  // Route to update database with new lake data
+  app.put("/api/usgs", (req, res) => {
+    db.model('Lake').updateMany(
+      { _id: req.params.id },
+      { }
+    )
+      .then(function (places) {
+        res.json(places);
+        console.log("Count increased");
+      })
+      .catch(function (err) {
+        res.json(err);
+      });
+  });
 
 };
