d076210c6084be74ca46e066891ef7dc41e0b5ae
John Yager <NCFatBoy@Johns-iMac.attlocal.net>
2018-12-12 23:27:48 -0500

    This contains the updates to synch the USGS and ACE data when the date/time do not match up

--- public/assets/js/lakes.js
+++ public/assets/js/lakes.js
@@ -25,13 +25,13 @@ const batch = [];
 let elevCheck = false;
 let flowCheck = false;
 
-// Function to make flow USGS call
+// Function to make elevation USGS call
 function elevUSGS() {
     // API call for flow
     $.ajax({
-        url: elevURL,
-        method: "GET",
-    })
+            url: elevURL,
+            method: "GET",
+        })
         .then(function (data) {
             console.log(data);
             // Set lake title on page
@@ -81,7 +81,7 @@ function elevUSGS() {
                 let element = dataValues[j];
                 let elev = element.value;
                 let splitTimeDate = element.dateTime.split("T");
-                let date = splitTimeDate[0];
+                let date = splitTimeDate[0].substring(2, 10).replace('-', ' ');
                 let time = splitTimeDate[1].substring(0, 5);
 
                 // Create the HTML Well (Section) and Add the table content for each reserved table
@@ -105,9 +105,9 @@ function elevUSGS() {
 function flowUSGS() {
     // API call for flow
     $.ajax({
-        url: flowURL,
-        method: "GET",
-    })
+            url: flowURL,
+            method: "GET",
+        })
         .then(function (data) {
             console.log(data);
             // Parse through the json data to find the values we want
@@ -143,20 +143,88 @@ function flowUSGS() {
 
 
 // Function to make Kerr ACE call
-function kerrFlowACE() {
+function kerrFlowACE(dataTables) {
     $.ajax({
-        url: "/api/kerr",
-        method: "GET"
-    })
+            url: "/api/kerr",
+            method: "GET"
+        })
         .then(function (data) {
-            console.log(data);
+
             // Check if date matches 
+            // Find first element in USGS data in which the time value that is at the top of the hour
+            switch (dataTables[0].dateTime.substring(14, 16)) {
+
+                case "00":
+                    var j = 0;
+                    break;
+
+                case "15":
+                    var j = 1;
+                    break;
+
+                case "30":
+                    var j = 2
+                    break;
+
+                case "45":
+                    var j = 3
+                    break;
+
+                default:
+                    alert("Lake name does not exists");
+            }
+
 
             let i = 0;
-            data.forEach(function (element) {
-                $("#lakeWell-" + i + 1).append("<td>" + element.outflow + "</td>");
-                i++;
-            })
+            let k = 0;
+            let aceTime = 0; // Time on current ACE line
+            let usgsTime = 0; // Time on current USGS line
+
+            // Code readability, will be set to comparison of times in loop
+            let timesAreNotEqual = false;
+            let aceTimeGreater = 0;
+            let monthsAreEqual = false;
+            let dataIsLinedUpByDate = false;
+
+            // Check to see if the first ACE flow data line is same time as USGS first line time and the same day 
+            // (don't care about month right now)
+            // Can fix the month problem later 
+            //(only an issue when site down as time goes by midnight last day of month)
+            for (j; j < dataTables.length; j += 4) {
+                if (data[i].time === '2400') { // ACE represents midnight as 2400 of passing day
+                    aceTime = '0000' // USGS represents midnight as 0000 of next day (Dates are different)
+                } else aceTime = data[i].time;
+
+                usgsTime = dataTables[j].dateTime.substring(11, 16).replace(':', ''); // Pulls the USGS time from the line and removes the : (ACE does not have a colon)
+
+                // Set Booleans for conditionals below, months have OR due to difference in representation of midnight
+                timesAreNotEqual = dataTables[j].dateTime.substring(11, 16).replace(':', '') !== aceTime;
+                aceTimeGreater = parseInt(dataTables[j].dateTime.substring(11, 16).replace(':', ''), 10) < parseInt(aceTime, 10);
+                monthsAreEqual = (data[i].date.substring(0, 2) == dataTables[j].dateTime.substring(8, 10)) || aceTime == '0000';
+
+                let element = data[i]; // define element for template updates below.
+
+                if (monthsAreEqual || dataIsLinedUpByDate) {
+                    if (timesAreNotEqual) {
+                        if (aceTimeGreater) { // is ACE Time newer than USGS (ACE has newer data?)
+                            j = j - 4; // Stay on current USGS line by decrementing loop counter
+                            k++; // Stay on current LakeWell Template line
+                        } else { // ACE Time is less than USGS Time (ACE site down)
+
+
+                            $("#lakeWell-" + (i - k) + 1).append("<td>" + "N/A" + "</td>"); //Append N/A as the Flow Value to the row for the missing data
+                            i--; // Stay on current ACE line by decrementing index
+                            k--; //Move to next Lakewell Template line
+                        }
+                    } else { // Times are equal, post data to Lakewell template
+
+                        $("#lakeWell-" + (i - k) + 1).append("<td>" + element.outflow + "</td>");
+                        dataIsLinedUpByDate = true;
+                    }
+                }
+
+                i++; // Increment loop counter ACE data and LakeWell template
+            }
         });
 }
 
@@ -164,11 +232,10 @@ function kerrFlowACE() {
 function kerrElevFlow() {
     // API call for flow
     $.ajax({
-        url: elevURL,
-        method: "GET",
-    })
+            url: elevURL,
+            method: "GET",
+        })
         .then(function (data) {
-            console.log(data);
             // Set lake title on page
             $("#lakeTitle").append(capitalizeFirstLetter(lakeName));
             // Parse the json data return to find the values we want
@@ -189,7 +256,7 @@ function kerrElevFlow() {
             $("#currentDelta").append(currentDelta);
 
 
-            // Find first time value that is at the top of the hour
+            // Find first element in USGS data in which the time value that is at the top of the hour
             switch (dataValues[0].dateTime.substring(14, 16)) {
 
                 case "00":
@@ -218,9 +285,9 @@ function kerrElevFlow() {
             for (j; j < dataValues.length; j += 4) {
                 let element = dataValues[j];
                 let elev = element.value;
-                let splitTimeDate = element.dateTime.split("T");
-                let date = splitTimeDate[0];
-                let time = splitTimeDate[1].substring(0, 5);
+
+                let date = element.dateTime.substring(2, 10).replace('-', ' ');
+                let time = element.dateTime.substring(11, 16);
 
                 // Create the HTML Well (Section) and Add the table content for each reserved table
                 var lakeSection = $("<tr>");
@@ -235,7 +302,7 @@ function kerrElevFlow() {
                 $("#lakeWell-" + i + 1).append("<td>" + elev + "</td>");
                 i++;
             }
-            kerrFlowACE();
+            kerrFlowACE(dataValues);
         })
 }
 
