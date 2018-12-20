7f5f213cc87380adfd1429dbd86f9f646fb62219
jmyager <jmyager6@gmail.com>
2018-12-20 16:12:43 -0500

    filter form reset to default when submit

--- public/assets/js/tournaments.js
+++ public/assets/js/tournaments.js
@@ -221,6 +221,8 @@ $(".btn-filter").on("click", function (e) {
         currentBatch = filteredBatch;
         filtered = true;
     }
+    // Reset the option field to the default (ex: "Select Org")
+    $(".default-option").prop({selected: true});
 });
 
 // // Function to create Parameter buttons inside parameter-well. Function is called after Add button function
