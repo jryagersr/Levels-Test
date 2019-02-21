42ad9644cfe02752602eb520f095027b2566c00c
John Yager <NCFatBoy@Johns-iMac.attlocal.net>
2019-02-21 12:42:22 -0500

    First pass at sponsorData.js

---
+++ data/sponsorData.js
@@ -0,0 +1,451 @@
+module.exports = [
+    // Comment added to make change
+
+    /************************************************************************************************************************************** */
+    {
+        sponsorName: "BoysandGirls",
+        logo: "png file",
+        defaultHref: "http://bassforcash.com/schedule/",
+        sponsorships: [{
+            name: "Boys and Girls Club of Henderson, NC",
+            type: [
+
+                {
+                    type: "charityTx", //This tells where to position the sponsor featuredTx, lake, charityTx, txPage, Banner, lakeTx, txPage
+                    lake: [
+                        "Kerr" // for lake or tx sponsor
+                    ],
+                    startDate: [""],
+                    endDate: [""],
+                    text: ["Kerr Lake April 20th"], // For featuredTx, lakeTx and charityTx
+                    stScope: ["NC, VA"], // States or All
+                    detail: ["Charity Tournament Combined with BFCS Tx"],
+                    link: ["http://bassforcash.com/schedule/"]
+                }
+            ]
+        }]
+    },
+    /************************************************************************************************************************************** */
+    {
+        sponsorName: "BrucatoFIS",
+        logo: "png file",
+        defaultHref: "http://www.BrucatoFIS.com",
+        sponsorships: [{
+                name: "Brucato Fuel Injector Service",
+                type: [
+
+                    {
+                        type: "Banner", //This tells where to position the sponsor featuredTx, lake, charityTx, txPage, Banner, lakeTx, txPage
+                        lake: [
+                            "All" // for lake or tx sponsor
+                        ],
+                        startDate: ["",
+                            "",
+                            ""
+                        ],
+                        endDate: ["", "", ""],
+                        text: ["All lake banner"], // For featuredTx, lakeTx and charityTx
+                        stScope: ["All"], // States or All
+                        detail: ["Bottom Banner All sponsor"],
+                        link: ["http://www.BrucatoFIS.com"]
+                    }
+                ]
+            },
+            {
+                name: "Brucato Fuel Injector Service",
+                type: [
+
+                    {
+                        type: "lake", //This tells where to position the sponsor featuredTx, lake, charityTx, txPage, Banner, lakeTx, txPage
+                        lake: [
+                            "Jordan" // for lake or tx sponsor
+                        ],
+                        startDate: [""],
+                        endDate: [""],
+                        text: ["lake Sponsor"], // For featuredTx, lakeTx and charityTx
+                        stScope: ["All"], // States or All
+                        detail: ["Bottom Banner All sponsor"],
+                        link: ["http://www.BrucatoFIS.com"]
+                    }
+                ]
+            }
+        ]
+    },
+    /************************************************************************************************************************************** */
+    {
+        sponsorName: "CATT",
+        logo: "png file",
+        defaultHref: "http://www.cattteamtrail.com",
+        sponsorships: [{
+                name: "Carolina Anglers Team Trail",
+                type: [
+
+                    {
+                        type: "featuredTx",
+                        lake: [
+                            "Kerr",
+                            "Falls",
+                            "Jordan"
+                        ],
+                        startDate: ["",
+                            "",
+                            ""
+                        ],
+                        endDate: ["",
+                            "",
+                            ""
+                        ],
+                        text: ["NC/SC Championship Kerr Lake June 1-2",
+                            " Old North Falls Qualifier March 23rd",
+                            "Old North Jordan Qualifier April 28th"
+                        ],
+                        stScope: ["SC, NC, VA, TN, GA",
+                            "SC, NC, VA, TN, GA",
+                            "SC, NC, VA, TN, GA"
+                        ],
+                        detail: ["Featured Tx - multiple Tx on multiple lakes",
+                            "Featured Tx - multiple Tx on multiple lakes",
+                            "Featured Tx - multiple Tx on multiple lakes"
+                        ],
+                        link: ["https://www.cattteamtrail.com/divisions/catt-10000-championship/",
+                            "https://www.cattteamtrail.com/divisions/catt-old-north/",
+                            "https://www.cattteamtrail.com/divisions/catt-old-north/"
+                        ]
+                    }
+                ]
+            }, {
+                name: "Carolina Anglers Team Trail",
+                type: [
+
+                    {
+                        type: "lakeTx",
+                        lake: [
+                            "Kerr",
+                            "Kerr",
+                            "Falls",
+                            "Jordan",
+                            "Jordan",
+                            "Kerr",
+                            "Jordan"
+                        ],
+                        startDate: ["",
+                            "",
+                            "",
+                            "",
+                            "",
+                            "",
+                            ""
+                        ],
+                        endDate: ["",
+                            "",
+                            "",
+                            "",
+                            "",
+                            "",
+                            ""
+                        ],
+                        text: ["CATT NC/SC Championship Kerr Lake June 1-2",
+                            "CATT Old North Kerr Qualifier March 9th",
+                            "CATT Old North Falls Qualifier March 23rd",
+                            "CATT Old North Jordan Qualifier April 28th",
+                            "CATT Old North Jordan QUalifier May 18th",
+                            "CATT Old North Kerr Qualifier June 8th",
+                            "CATT Old North Final Jordan June 29"
+                        ],
+                        stScope: ["SC, NC, VA, TN, GA",
+                            "SC, NC, VA, TN, GA",
+                            "SC, NC, VA, TN, GA",
+                            "SC, NC, VA, TN, GA",
+                            "SC, NC, VA, TN, GA",
+                            "SC, NC, VA, TN, GA",
+                            "SC, NC, VA, TN, GA"
+                        ],
+                        detail: ["lake Tx - multiple Tx on multiple lakes"],
+                        link: ["https://www.cattteamtrail.com/divisions/catt-10000-championship/",
+                            "https://www.cattteamtrail.com/divisions/catt-old-north/",
+                            "https://www.cattteamtrail.com/divisions/catt-old-north/",
+                            "https://www.cattteamtrail.com/divisions/catt-old-north/",
+                            "https://www.cattteamtrail.com/divisions/catt-old-north/",
+                            "https://www.cattteamtrail.com/divisions/catt-old-north/",
+                            "https://www.cattteamtrail.com/divisions/catt-old-north/"
+                        ]
+                    }
+                ]
+            },
+            {
+                name: "Carolina Anglers Team Trail",
+                type: [
+
+                    {
+                        type: "lake",
+                        lake: [
+                            "Kerr",
+                            "Jordan",
+                            "Falls",
+                            "Wateree",
+                            "Murray",
+                            "Hartwell"
+                        ],
+                        startDate: ["",
+                            "",
+                            "",
+                            "",
+                            "",
+                            ""
+                        ],
+                        endDate: ["",
+                            "",
+                            "",
+                            "",
+                            "",
+                            ""
+                        ],
+                        text: [""],
+                        stScope: ["SC, NC, VA, TN, GA",
+                            "SC, NC, VA, TN, GA",
+                            "SC, NC, VA, TN, GA",
+                            "SC, NC, VA, TN, GA",
+                            "SC, NC, VA, TN, GA",
+                            "SC, NC, VA, TN, GA"
+                        ],
+                        detail: [""],
+                        link: ["https://www.cattteamtrail.com/divisions/catt-10000-championship/",
+                            "https://www.cattteamtrail.com/divisions/catt-old-north/",
+                            "https://www.cattteamtrail.com/divisions/catt-old-north/",
+                            "https://www.cattteamtrail.com/divisions/catt-lake-wateree/",
+                            "https://www.cattteamtrail.com/divisions/catt-lake-murray/",
+                            "https://www.cattteamtrail.com/divisions/catt-lake-hartwell/"
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    /************************************************************************************************************************************** */
+    {
+        sponsorName: "Collins",
+        logo: "png file",
+        defaultHref: "http://CollinsBoating.com/",
+        sponsorships: [{
+                name: "Collins, Inc",
+                type: [
+
+                    {
+                        type: "Banner",
+                        lake: [
+                            "Kerr",
+                            "Falls",
+                            "Jordan",
+                            "Gaston"
+                        ],
+                        startDate: ["",
+                            "",
+                            "",
+                            ""
+                        ],
+                        endDate: ["",
+                            "",
+                            "",
+                            ""
+                        ],
+                        text: ["",
+                            "",
+                            "",
+                            ""
+                        ],
+                        stScope: ["NC"],
+                        detail: ["Bottom Banner inclusion",
+                            "Bottom Banner inclusion",
+                            "Bottom Banner inclusion",
+                            "Bottom Banner inclusion"
+                        ],
+                        link: ["http://jacksonsuperiorelectric.com/",
+                            "http://jacksonsuperiorelectric.com/",
+                            "http://jacksonsuperiorelectric.com/",
+                            "http://jacksonsuperiorelectric.com/"
+                        ]
+                    }
+                ]
+            },
+            {
+                name: "Collins, Inc",
+                type: [
+
+                    {
+                        type: "lake",
+                        lake: [
+                            "Kerr",
+                            "Falls",
+                            "Jordan"
+                        ],
+                        startDate: ["",
+                            "",
+                            ""
+                        ],
+                        endDate: ["",
+                            "",
+                            ""
+                        ],
+                        text: ["",
+                            "",
+                            ""
+                        ],
+                        stScope: ["NC",
+                            "NC",
+                            "NC"
+                        ],
+                        detail: ["Lake Sponsor",
+                            "Lake Sponsor",
+                            "Lake Sponsor"
+                        ],
+                        link: ["http://CollinsBoating.com/",
+                            "http://CollinsBoating.com/",
+                            "http://CollinsBoating.com/",
+                        ]
+                    }
+                ]
+            },
+            {
+                name: "Bass For Cash Series",
+                type: [
+
+                    {
+                        type: "lakeTx",
+                        lake: [
+                            "Jordan",
+                            "Kerr"
+                        ],
+                        startDate: ["",
+                            ""
+                        ],
+                        endDate: ["",
+                            ""
+                        ],
+                        text: ["Jordan Lake March 30th",
+                            "Kerr Lake April 20th"
+                        ],
+                        stScope: ["NC",
+                            "NC"
+                        ],
+                        detail: ["lakeTx sponsor",
+                            "lakeTx sponsor"
+                        ]
+                        link: ["http://CollinsBoating.com/",
+                            "http://CollinsBoating.com/"
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+
+    /************************************************************************************************************************************** */
+    {
+        sponsorName: "FOM/BPS",
+        logo: "png file",
+        defaultHref: "",
+        sponsorships: [{
+            name: "Warriors On The Water",
+            type: [{
+                type: "charityTx", //This tells where to position the sponsor featuredTx, lake, charityTx, txPage, Banner, lakeTx, txPage
+                lake: [
+                    "Jordan" // for lake or tx sponsor
+                ],
+                startDate: [""],
+                endDate: [""],
+                text: ["Jordan Lake May 10th"],
+                stScope: ["NC"], // States or All
+                detail: ["Charity Tournament"],
+                link: ["http://www.WarriorsOntheWater.com"]
+            }]
+        }]
+    },
+    /************************************************************************************************************************************** */
+    {
+        sponsorName: "JSE",
+        logo: "png file",
+        defaultHref: "http://jacksonsuperiorelectric.com/",
+        sponsorships: [{
+                name: "Jackson Superior Electric",
+                type: [
+
+                    {
+                        type: "Banner",
+                        lake: [
+                            "Kerr",
+                            "Falls",
+                            "Jordan",
+                            "Gaston"
+                        ],
+                        startDate: ["",
+                            "",
+                            "",
+                            ""
+                        ],
+                        endDate: ["",
+                            "",
+                            "",
+                            ""
+                        ],
+                        text: ["",
+                            "",
+                            "",
+                            ""
+                        ],
+                        stScope: ["NC",
+                            "NC",
+                            "NC",
+                            "NC"
+                        ],
+                        detail: ["Bottom Banner inclusion",
+                            "Bottom Banner inclusion",
+                            "Bottom Banner inclusion",
+                            "Bottom Banner inclusion"
+                        ],
+                        link: ["http://jacksonsuperiorelectric.com/",
+                            "http://jacksonsuperiorelectric.com/",
+                            "http://jacksonsuperiorelectric.com/",
+                            "http://jacksonsuperiorelectric.com/"
+                        ]
+                    }
+                ]
+            },
+            {
+                name: "Jackson Superior Electric",
+                type: [{
+                    type: "lake",
+                    lake: [
+                        "Kerr"
+                    ],
+                    startDate: [""],
+                    endDate: [""],
+                    text: [""],
+                    stScope: ["NC"],
+                    detail: ["Lake Sponsor"],
+                    link: ["http://jacksonsuperiorelectric.com/"]
+                }]
+            }
+        ]
+    },
+    /************************************************************************************************************************************** */
+    {
+        sponsorName: "WoW",
+        logo: "png file",
+        defaultHref: "http://www.WarriorsOntheWater.com",
+        sponsorships: [{
+            name: "Warriors On The Water",
+            type: [{
+                type: "charityTx", //This tells where to position the sponsor featuredTx, lake, charityTx, txPage, Banner, lakeTx, txPage
+                lake: [
+                    "Jordan" // for lake or tx sponsor
+                ],
+                startDate: [""],
+                endDate: [""],
+                text: ["Jordan Lake May 10th"],
+                stScope: ["NC"], // States or All
+                detail: ["Charity Tournament"],
+                link: ["http://www.WarriorsOntheWater.com"]
+            }]
+        }]
+    }
+]
