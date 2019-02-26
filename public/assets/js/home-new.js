3c0b81c02aa8779555a057f6ab3fb15972f0b9e7
jmyager <jmyager6@gmail.com>
2019-02-26 16:24:14 -0500

    location service update. only NC lakes enabled

--- public/assets/js/home-new.js
+++ public/assets/js/home-new.js
@@ -1,38 +1,108 @@
+let lat;
+let lon;
+let kerrLat = 36.588792;
+let kerrLon = -78.352668;
+let closeLakes = [];
+let oldDistance = 0;
+let newDistance = 0;
+var x = document.getElementById("noLocation");
+let lakeData;
+let lakeTemplate = '';
+
+// get all of our lake data
 $.ajax({
     url: "/api/lake-data",
     method: "GET",
 })
     .then(function (data) {
         console.log(data);
-        let closeLakes = [];
-        let distance = 0;
-        let newDistance = 0;
-        for (var i = 0; i < data.length; i++) {
-            newDistance = distance(lat, lon, data.lat, data.lon, "M");
-            if (newDistance < distance) {
-                closeLakes.push(data.bodyofWaterName);
-            }
-        }
-    });
+        lakeData = data;
 
-        var x = document.getElementById("demo");
-        let lat;
-        let lon;
-        function getLocation() {
+        // run get location function
+        getLocation(function (lat, lon) {
+
+            // loop through lake data (right now only NC)
+            lakeData[19].lakes.forEach(function (lake) {
+                // calculate our distance between user and each lake
+                newDistance = distance(lat, lon, lake.lat, lake.long, "M")
+                // collect the first 10 regardless
+                if (closeLakes.length < 10) {
+                    closeLakes.push({
+                        name: lake.bodyOfWater,
+                        distance: newDistance,
+                        href: lake.href
+                    });
+                }
+                // if ten have already been collected
+                else {
+                    // loop through those ten
+                    for (var i = 0; i < closeLakes.length; i++) {
+                        // and check if the next lake we're looking at is closer
+                        if (closeLakes[i].distance > newDistance) {
+                            // if it is splice it into closeLakes (while removing the larger one);
+                            closeLakes.splice(i, 1, { name: lake.bodyOfWater, distance: newDistance, href: lake.href });
+                            break;
+                        }
+                    }
+                }
+            });
+
+            // sort by ascending distance
+            console.log(closeLakes);
+            closeLakes = closeLakes.sort(function (a, b) { return (a.distance - b.distance) });
+            // loop through closeLakes and build the template for the page
+            closeLakes.forEach(function (lake) {
+                lakeTemplate += `
+                <a href=${lake.href}>
+                    <div class="lake-card">
+                        <h2>${lake.name}</h2>
+                        <p>${lake.distance.toFixed(0)} miles away</p>
+                    </div>
+                </a>`;
+            });
+            // append template to page
+            $('#lakeContainer').append(lakeTemplate);
+        });
+
+
+
+
+        // function to get user's location
+        function getLocation(callback) {
             if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(showPosition);
+                navigator.geolocation.getCurrentPosition(function (position) {
+                    callback(position.coords.latitude, position.coords.longitude)
+                },  showError);
             } else {
                 x.innerHTML = "Geolocation is not supported by this browser.";
             }
         }
+ 
 
-        function showPosition(position) {
-            lat = position.coords.latitude;
-            lon = position.coords.longitude;
-            console.log(lat + ", " + lon);
-            console.log(distance(lat, lon, kerrLat, kerrLon, "M"));
+        // function to define errors for geolocation (ex: user denies location access)
+        function showError(error) {
+            switch (error.code) {
+                case error.PERMISSION_DENIED:
+                    x.innerHTML = "<p>You've opted out of using location services.</p><a href='/lakes' class='btn btn-success'>View All Lakes</a>"
+                    break;
+                case error.POSITION_UNAVAILABLE:
+                    x.innerHTML = "Location information is unavailable."
+                    break;
+                case error.TIMEOUT:
+                    x.innerHTML = "The request to get user location timed out."
+                    break;
+                case error.UNKNOWN_ERROR:
+                    x.innerHTML = "An unknown error occurred."
+                    break;
+            }
         }
 
+        // function to calculate distance between two locations
+        // https://www.geodatasource.com/developers/javascript
+        // unit = the unit you desire for results
+        // where: 'M' is statute miles (default)
+        // 'K' is kilometers
+        // 'N' is nautical miles
         function distance(lat1, lon1, lat2, lon2, unit) {
             if ((lat1 == lat2) && (lon1 == lon2)) {
                 return 0;
@@ -55,5 +125,4 @@ $.ajax({
             }
         }
 
-        let kerrLat = 36.588792;
-        let kerrLon = -78.352668;
+    });
--- public/assets/js/sponsor.js
+++ public/assets/js/sponsor.js
@@ -1,184 +1,184 @@
-// // Advertisements
-// // ===============================================================
+// // // Advertisements
+// // // ===============================================================
 
-var bottomBannerSponsors = [
-    {
-        sponsor: "JSE",
-        href: "http://jacksonsuperiorelectric.com/",
-        src: "/static/assets/img/sponsors/JSE-logo-660px.png"
-    },
-    {
-        sponsor: "BrucatoFis", // All
-        href: "http://www.brucatofis.com/",
-        src: "/static/assets/img/sponsors/brucatonewlogo-660px.png"
-    },
-    {
-        sponsor: "BFCS",
-        href: "https://www.bassforcash.com/",
-        src: "/static/assets/img/sponsors/bfcs logo.png"
-    },
-    {
-        sponsor: "CBC",
-        href: "https://www.cattteamtrail.com/",
-        src: "/static/assets/img/sponsors/CBC-logo-660px.png"
-    },
-    {
-        sponsor: "Collins",
-        href: "http://collinsboating.com/",
-        src: "/static/assets/img/sponsors/collinslogo.png"
-    },
-    {
-        sponsor: "Outhouse",
-        href: "http://www.outhousetackle.com/",
-        src: "/static/assets/img/sponsors/outhouse-tacklelogo.png"
-    },
-    {
-        sponsor: "DeepCreek", // All
-        href: "http://deepcreeklures.com/",
-        src: "/static/assets/img/sponsors/deepcreeklures logo.png"
-    },
-    {
-        sponsor: "CATT",
-        href: "https://www.cattteamtrail.com/",
-        src: "/static/assets/img/sponsors/catt logo.png"
-    },
-    {
-        sponsor: "Academy", // All
-        href: "https://www.academy.com/",
-        src: "/static/assets/img/sponsors/academy logo.png"
-    },
+// var bottomBannerSponsors = [
+//     {
+//         sponsor: "JSE",
+//         href: "http://jacksonsuperiorelectric.com/",
+//         src: "/static/assets/img/sponsors/JSE-logo-660px.png"
+//     },
+//     {
+//         sponsor: "BrucatoFis", // All
+//         href: "http://www.brucatofis.com/",
+//         src: "/static/assets/img/sponsors/brucatonewlogo-660px.png"
+//     },
+//     {
+//         sponsor: "BFCS",
+//         href: "https://www.bassforcash.com/",
+//         src: "/static/assets/img/sponsors/bfcs logo.png"
+//     },
+//     {
+//         sponsor: "CBC",
+//         href: "https://www.cattteamtrail.com/",
+//         src: "/static/assets/img/sponsors/CBC-logo-660px.png"
+//     },
+//     {
+//         sponsor: "Collins",
+//         href: "http://collinsboating.com/",
+//         src: "/static/assets/img/sponsors/collinslogo.png"
+//     },
+//     {
+//         sponsor: "Outhouse",
+//         href: "http://www.outhousetackle.com/",
+//         src: "/static/assets/img/sponsors/outhouse-tacklelogo.png"
+//     },
+//     {
+//         sponsor: "DeepCreek", // All
+//         href: "http://deepcreeklures.com/",
+//         src: "/static/assets/img/sponsors/deepcreeklures logo.png"
+//     },
+//     {
+//         sponsor: "CATT",
+//         href: "https://www.cattteamtrail.com/",
+//         src: "/static/assets/img/sponsors/catt logo.png"
+//     },
+//     {
+//         sponsor: "Academy", // All
+//         href: "https://www.academy.com/",
+//         src: "/static/assets/img/sponsors/academy logo.png"
+//     },
     
-    {
-        sponsor: "Daves", // All
-        href: "https://www.davestournamenttackle.com/",
-        src: "/static/assets/img/sponsors/daves logo.png"
-    },
-    {
-        sponsor: "TrueSouth", // All
-        href: "https://www.truesouthlures.com/",
-        src: "/static/assets/img/sponsors/true south logo.png"
-    },
-    {
-        sponsor: "Siebert", //All
-        href: "http://www.siebertoutdoors.com",
-        src: "/static/assets/img/sponsors/siebert outdoors logo.png"
-    },
-    {
-        sponsor: "Taterhog", //All
-        href: "http://www.taterhoglures.com",
-        src: "/static/assets/img/sponsors/tater hog logo.png"
-    },
-    {
-        sponsor: "Foothills",// All
-        href: "https://www.foothillsmarine.com/",
-        src: "/static/assets/img/sponsors/foothills logo.png"
-    },
-    {
-        sponsor: "Flatlands", // All
-        href: "http://www.flatlandstackle.com",
-        src: "/static/assets/img/sponsors/flatlands logo.png"
-    },
-    {
-        sponsor: "TRC",// All
-        href: "http://www.trccovers.com",
-        src: "/static/assets/img/sponsors/trc logo.png"
-    },
-    {
-        sponsor: "Bobcats",// All
-        href: "http://www.bobcatslakecountry.com/",
-        src: "/static/assets/img/sponsors/bobcat logo.png"
-    },
-    {
-        sponsor: "BBS",// All
-        href: "https://www.bassboatsaver.com/",
-        src: "/static/assets/img/sponsors/bbs logo.png"
-    },
-    {
-        sponsor: "Level",// All
-        href: "https://level-rods.myshopify.com/",
-        src: "/static/assets/img/sponsors/level logo.png"
-    },
-    {
-        sponsor: "AnglersChoice",// All
-        href: "https://www.anglerschoicemarine.com/",
-        src: "/static/assets/img/sponsors/anglers choice logo.png"
-    }
-    ];
+//     {
+//         sponsor: "Daves", // All
+//         href: "https://www.davestournamenttackle.com/",
+//         src: "/static/assets/img/sponsors/daves logo.png"
+//     },
+//     {
+//         sponsor: "TrueSouth", // All
+//         href: "https://www.truesouthlures.com/",
+//         src: "/static/assets/img/sponsors/true south logo.png"
+//     },
+//     {
+//         sponsor: "Siebert", //All
+//         href: "http://www.siebertoutdoors.com",
+//         src: "/static/assets/img/sponsors/siebert outdoors logo.png"
+//     },
+//     {
+//         sponsor: "Taterhog", //All
+//         href: "http://www.taterhoglures.com",
+//         src: "/static/assets/img/sponsors/tater hog logo.png"
+//     },
+//     {
+//         sponsor: "Foothills",// All
+//         href: "https://www.foothillsmarine.com/",
+//         src: "/static/assets/img/sponsors/foothills logo.png"
+//     },
+//     {
+//         sponsor: "Flatlands", // All
+//         href: "http://www.flatlandstackle.com",
+//         src: "/static/assets/img/sponsors/flatlands logo.png"
+//     },
+//     {
+//         sponsor: "TRC",// All
+//         href: "http://www.trccovers.com",
+//         src: "/static/assets/img/sponsors/trc logo.png"
+//     },
+//     {
+//         sponsor: "Bobcats",// All
+//         href: "http://www.bobcatslakecountry.com/",
+//         src: "/static/assets/img/sponsors/bobcat logo.png"
+//     },
+//     {
+//         sponsor: "BBS",// All
+//         href: "https://www.bassboatsaver.com/",
+//         src: "/static/assets/img/sponsors/bbs logo.png"
+//     },
+//     {
+//         sponsor: "Level",// All
+//         href: "https://level-rods.myshopify.com/",
+//         src: "/static/assets/img/sponsors/level logo.png"
+//     },
+//     {
+//         sponsor: "AnglersChoice",// All
+//         href: "https://www.anglerschoicemarine.com/",
+//         src: "/static/assets/img/sponsors/anglers choice logo.png"
+//     }
+//     ];
 
-// Clear any ad content in the scroller
-$("#adScrollerBottom").empty();
+// // Clear any ad content in the scroller
+// $("#adScrollerBottom").empty();
 
-// Loop through our ads and append them to the page in format: <li><a><img></a></li>
+// // Loop through our ads and append them to the page in format: <li><a><img></a></li>
 
-bottomBannerSponsors.forEach(function(element) {
-    var li = $("<li>");
-    var a = $("<a target='_blank'>");
-    a.attr("href", element.href);
-    var adImg = $("<img class='ad-scroller-img'>");
-    adImg.attr("src", element.src);
-    $("#adScrollerBottom").append(li);
-    $(li).append(a);
-    $(a).append(adImg);
-});
+// bottomBannerSponsors.forEach(function(element) {
+//     var li = $("<li>");
+//     var a = $("<a target='_blank'>");
+//     a.attr("href", element.href);
+//     var adImg = $("<img class='ad-scroller-img'>");
+//     adImg.attr("src", element.src);
+//     $("#adScrollerBottom").append(li);
+//     $(li).append(a);
+//     $(a).append(adImg);
+// });
 
 
-// Function code for bottom banner scroller
+// // Function code for bottom banner scroller
 
-$(function(){
-    var scroller = $('#scroller div.innerScrollArea');
-    var scrollerContent = scroller.children('ul');
-    scrollerContent.children().clone().appendTo(scrollerContent);
-    var curX = 0;
-    scrollerContent.children().each(function(){
-        var $this = $(this);
-        $this.css('left', curX);
-        curX += $this.outerWidth(true);
-    });
-    var fullW = curX / 2;
-    var viewportW = scroller.width();
+// $(function(){
+//     var scroller = $('#scroller div.innerScrollArea');
+//     var scrollerContent = scroller.children('ul');
+//     scrollerContent.children().clone().appendTo(scrollerContent);
+//     var curX = 0;
+//     scrollerContent.children().each(function(){
+//         var $this = $(this);
+//         $this.css('left', curX);
+//         curX += $this.outerWidth(true);
+//     });
+//     var fullW = curX / 2;
+//     var viewportW = scroller.width();
 
-    scroller.css('overflow-x', 'auto');
-});
+//     scroller.css('overflow-x', 'auto');
+// });
 
-$(function(){
-    var scroller = $('#scroller div.innerScrollArea');
-    var scrollerContent = scroller.children('ul');
-    scrollerContent.children().clone().appendTo(scrollerContent);
-    var curX = 0;
-    scrollerContent.children().each(function(){
-        var $this = $(this);
-        $this.css('left', curX);
-        curX += $this.outerWidth(true);
-    });
-    var fullW = curX / 2;
-    var viewportW = scroller.width();
+// $(function(){
+//     var scroller = $('#scroller div.innerScrollArea');
+//     var scrollerContent = scroller.children('ul');
+//     scrollerContent.children().clone().appendTo(scrollerContent);
+//     var curX = 0;
+//     scrollerContent.children().each(function(){
+//         var $this = $(this);
+//         $this.css('left', curX);
+//         curX += $this.outerWidth(true);
+//     });
+//     var fullW = curX / 2;
+//     var viewportW = scroller.width();
 
-    // Scrolling speed management
-    var controller = {curSpeed:0, fullSpeed:1};
-    var $controller = $(controller);
-    var tweenToNewSpeed = function(newSpeed, duration)
-    {
-        if (duration === undefined)
-            duration = 600;
-        $controller.stop(true).animate({curSpeed:newSpeed}, duration);
-    };
+//     // Scrolling speed management
+//     var controller = {curSpeed:0, fullSpeed:1};
+//     var $controller = $(controller);
+//     var tweenToNewSpeed = function(newSpeed, duration)
+//     {
+//         if (duration === undefined)
+//             duration = 600;
+//         $controller.stop(true).animate({curSpeed:newSpeed}, duration);
+//     };
 
-    // Pause on hover
-    scroller.hover(function(){
-        tweenToNewSpeed(0);
-    }, function(){
-        tweenToNewSpeed(controller.fullSpeed);
-    });
+//     // Pause on hover
+//     scroller.hover(function(){
+//         tweenToNewSpeed(0);
+//     }, function(){
+//         tweenToNewSpeed(controller.fullSpeed);
+//     });
 
-    // Scrolling management; start the automatical scrolling
-    var doScroll = function()
-    {
-        var curX = scroller.scrollLeft();
-        var newX = curX + controller.curSpeed;
-        if (newX > fullW*2 - viewportW)
-            newX -= fullW;
-        scroller.scrollLeft(newX);
-    };
-    setInterval(doScroll, 20);
-    tweenToNewSpeed(controller.fullSpeed);
-});
+//     // Scrolling management; start the automatical scrolling
+//     var doScroll = function()
+//     {
+//         var curX = scroller.scrollLeft();
+//         var newX = curX + controller.curSpeed;
+//         if (newX > fullW*2 - viewportW)
+//             newX -= fullW;
+//         scroller.scrollLeft(newX);
+//     };
+//     setInterval(doScroll, 20);
+//     tweenToNewSpeed(controller.fullSpeed);
+// });
