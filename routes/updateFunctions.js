a2e837a08e7f294eb7104fb628bf6d9988ebf122
jmyager <jmyager6@gmail.com>
2019-05-13 15:09:03 -0400

    dupe entry bugfix

--- routes/updateFunctions.js
+++ routes/updateFunctions.js
@@ -1,23 +1,23 @@
 const db = require("../models")();
- // Import all data source update functions
- const ace = require("./updateRoutes/getACEData");
- const cube = require("./updateRoutes/getCUBEData");
- const duke = require("./updateRoutes/getDUKEData");
- const sjrwmd = require("./updateRoutes/getSJRWMDData");
- const tva = require("./updateRoutes/getTVAData");
- const twdb = require("./updateRoutes/getTWDBData");
- const usgs = require("./updateRoutes/getUSGSData");
- const uslakes = require("./updateRoutes/getUSLAKESData");
+// Import all data source update functions
+const ace = require("./updateRoutes/getACEData");
+const cube = require("./updateRoutes/getCUBEData");
+const duke = require("./updateRoutes/getDUKEData");
+const sjrwmd = require("./updateRoutes/getSJRWMDData");
+const tva = require("./updateRoutes/getTVAData");
+const twdb = require("./updateRoutes/getTWDBData");
+const usgs = require("./updateRoutes/getUSGSData");
+const uslakes = require("./updateRoutes/getUSLAKESData");
 
 
 module.exports = {
 
-// ===============================================================================
-// UPDATE FUNCTIONS
-// ===============================================================================
+  // ===============================================================================
+  // UPDATE FUNCTIONS
+  // ===============================================================================
 
-// check to see if an update is needed (true = update is needed);
-checkForUpdate: function (lastRefresh, refreshInterval, dataLength) {
+  // check to see if an update is needed (true = update is needed);
+  checkForUpdate: function (lastRefresh, refreshInterval, dataLength) {
     // set today's date for comparison and find minute difference
     let today = new Date();
     let diffMins = 2400; // default setting to force update
@@ -38,31 +38,31 @@ checkForUpdate: function (lastRefresh, refreshInterval, dataLength) {
       return false;
     }
   },
-  
+
   // function to update and return one lake
-updateAndReturnOneLake: function (bodyOfWater, lastRefresh, data, callback) {
+  updateAndReturnOneLake: function (bodyOfWater, lastRefresh, data, callback) {
     // if new data exists, set the last Refresh time
     if (data.length > 0) {
       lastRefresh = data[0].time;
     }
     // use updateData to update the lake data
     db.model("Lake").findOneAndUpdate({
-        "bodyOfWater": bodyOfWater
-      }, 
+      "bodyOfWater": bodyOfWater
+    },
       // {
       //   $addToSet: {
       //     "data": {
       //       $each: data
       //     },
       //   },
-        {
-          $push: {
-            "data": {
-               $each: data,
-               $sort: { date: -1 },
-               position: 0
-            }
-          },
+      {
+        $push: {
+          "data": {
+            $each: data,
+            $sort: { date: -1 },
+            position: 0
+          }
+        },
         $set: {
           "lastRefresh": lastRefresh
         }
@@ -81,13 +81,16 @@ updateAndReturnOneLake: function (bodyOfWater, lastRefresh, data, callback) {
             // to get a value that is either negative, positive, or zero.
             return new Date(b.time) - new Date(a.time);
           });
-          // loop through data once more
-          for (var i = 1; i < updatedLake.data.length; i++) {
-            // check to see if there are two duplicate entrys
-            // convert timestamps to strings to avoid millisecond differences
-            if (updatedLake.data[i].time.toString() == updatedLake.data[i-1].time.toString()) {
-              // remove the oldest entry
-              updatedLake.data.splice(i,1);
+          // while the first two entries still have dupes
+          while (updatedLake.data[1].time.toString() == updatedLake.data[0].time.toString()) {
+            // loop through the data, beginning at first index
+            for (var i = 1; i < updatedLake.data.length; i++) {
+              // check to see if there are two duplicate entrys
+              // convert timestamps to strings to avoid millisecond differences
+              if (updatedLake.data[i].time.toString() == updatedLake.data[i - 1].time.toString()) {
+                // remove the oldest entry
+                updatedLake.data.splice(i - 1, 1);
+              }
             }
           }
           // log that the lake was updated and return it
@@ -96,9 +99,9 @@ updateAndReturnOneLake: function (bodyOfWater, lastRefresh, data, callback) {
           // update the database with the 'clean' data
           db.model("Lake").updateOne(
             { 'bodyOfWater': bodyOfWater },
-            { $set: { "data": updatedLake.data }}
+            { $set: { "data": updatedLake.data } }
           )
-            .exec(function(err) {
+            .exec(function (err) {
               if (err) {
                 console.log(error);
               }
