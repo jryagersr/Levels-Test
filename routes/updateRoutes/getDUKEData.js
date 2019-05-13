3bbd3f1a95a2c65a4d1ecbf782e6c8225cc718b0
jmyager <jmyager6@gmail.com>
2019-05-13 15:13:19 -0400

    Duke 0 value fix

--- routes/updateRoutes/getDUKEData.js
+++ routes/updateRoutes/getDUKEData.js
@@ -25,11 +25,7 @@ module.exports = {
                 // Check against future dates DUKE likes to send
                 if (new Date(lake.Date) <= today) {
                     // Check to make sure data is good
-                    if (lake.Average !== "N/A" && typeof parseInt(lake.Average) == 'number') {
-                        // if value isn't zero add the SLD
-                        if (Number(lake.Average) !== 0) {
-                            lake.Average = Number(lake.Average) + seaLevelDelta;
-                        }
+                    if (lake.Average !== "N/A" && typeof parseInt(lake.Average) == 'number' && Number(lake.Average) !== 0) {
                         data.push({
                             time: new Date(lake.Date + " " + "6:00"), // format timestamp
                             elev: Number(lake.Average), // add SLD to average
