95100dea7e71f7afd0af3dfaf0b60124a9771ab7
John Yager <NCFatBoy@Johns-iMac.attlocal.net>
2020-02-04 11:03:16 -0500

    Try again

--- data/lakeData.js
+++ data/lakeData.js
@@ -3105,7 +3105,7 @@ module.exports = [
         //elevURL: "http://water.usace.army.mil/a2w/CWMS_CRREL.cwms_data_api.get_report_json?p_location_id=1749041&p_parameter_type=Flow%3AStor%3APrecip%3AStage%3AElev&p_last=5&p_last_unit=days&p_unit_system=EN&p_format=JSON",
         flowURL: "",
         href: "/lakes/kerr",
-        //dataSource: ["USGS", "loadAds"], // array of calls to make to retrieve data
+        //dataSource: ["ACE", "loadAds"], // array of calls to make to retrieve data
         dataSource: ["USGS", "loadAds"], // array of calls to make to retrieve data
     },
     {
