adcbb9dfa5c38ad1d3d9ae551bbc9159d5ef7cab
John Yager <NCFatBoy@Johns-iMac.attlocal.net>
2020-10-14 08:24:48 -0400

    debugging

--- routes/dbRoutes.js
+++ routes/dbRoutes.js
@@ -26,7 +26,6 @@ var update = require('./updateFunctions');
 // // Connect to the Mondo DB
 const databaseUri = 'mongodb://localhost/BassSavvyTestDb';
 
-console.log("Mongoose attempting to connect with " + process.env.MONGODB_URI)
 
 if (process.env.MONGODB_URI) {
   db.connect(process.env.MONGODB_URI, {
